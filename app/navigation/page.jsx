c4d1b51bff0e989c669cd927c01d1e1cf52cbbe0
tugba-koc <koctug@itu.edu.tr>
2023-03-31 12:31:00 +0300

    feat: add navigation

---
+++ app/navigation/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navigation = () => {
+  return (
+    <nav className='flex mb-8'>
+      <li className='mr-5 underline list-none'>Home</li>
+      <li className='mx-5 underline list-none'>About</li>
+      <li className='mx-5 underline list-none'>Contact</li>
+    </nav>
+  );
+};
+
+export default Navigation;
